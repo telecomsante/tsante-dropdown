703fee8ec0d29fcc2e3c8d61acdcfe0c2bb4a5a9
Fabrice Le Coz <fabrice.lecoz@telecomsante.com>
2018-05-30 16:32:29 +0200

    fix import path

--- tsante-dropdown.js
+++ tsante-dropdown.js
@@ -37,9 +37,9 @@ example :
   from HTML and may be out of place here. Review them and
   then delete this comment!
 */
-import { PolymerElement } from '../@polymer/polymer/polymer-element.js';
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 
-import { afterNextRender } from '../@polymer/polymer/lib/utils/render-status.js';
+import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 const $_documentContainer = document.createElement('template');
 $_documentContainer.setAttribute('style', 'display: none;');
 
